c016b8ab50ac3b4653f59124d740a250a5c7ca03
zeeshanak23 <zakhter449@gmail.com>
2024-03-26 17:00:24 +0530

    added Tshirt section

--- src/components/seamlessDress/Seamless.tsx
+++ src/components/seamlessDress/Seamless.tsx
@@ -3,14 +3,14 @@ const Seamless = () => {
     <section>
       <div>
         <img
-          className=' h-[100vh] object-cover'
-          src='/images/seamless.webp'
+          className=' h-[100vh]  w-[100vw] object-cover'
+          src='/images/seamless.avif'
           alt=''
         />
       </div>
       <div className='absolute left-20 top-[200px] z-30 w-[400px]'>
-        <h1 className='text-5xl'>40% off New Arrival Seamless</h1>
-        <p className='mt-4 text-base text-[#666]'>
+        <h1 className='text-5xl'>40% off New Arrival T-Shirt</h1>
+        <p className='mt-4 text-base font-semibold '>
           Dressed to be adored And it’s all just gone on sale
         </p>
         <button className='mt-5 cursor-pointer rounded-full bg-black px-8 py-2  font-semibold uppercase text-white hover:opacity-70'>
