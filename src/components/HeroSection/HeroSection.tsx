98993d3a67b2670cbc5f72f6b722283cd6884ef0
zeeshanak23 <zakhter449@gmail.com>
2024-03-26 16:50:55 +0530

    added minor changes in css

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -11,15 +11,15 @@ const HeroSection = () => {
         />
       </div>
       <div className='absolute left-14 top-40 z-30 mt-5  text-white'>
-        <div className='w-[650px] text-center uppercase'>
+        <div className=' uppercase'>
           <h1 className='text-5xl'>MZAA</h1>
           <h1 className='mt-2 text-6xl'>Believe in your art</h1>
         </div>
-        <div className='ml-4'>
-          <p className='mt-8 text-lg'>
+        <div>
+          <p className='mt-6 text-lg font-medium'>
             I always said punk was an attitude And Creativity takes courage.
           </p>
-          <button className='mt-6 rounded-full bg-white px-8 py-3 font-semibold uppercase text-black hover:opacity-95'>
+          <button className='mt-6 rounded-full bg-white px-8 py-3  font-semibold uppercase text-black hover:opacity-95'>
             find out more
           </button>
         </div>
